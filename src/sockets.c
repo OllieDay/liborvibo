cd080b4ad0859b18159a6cdf770bddd5812de1bb
Ollie Day <OllieDay@users.noreply.github.com>
2015-11-19 20:19:07 +0000

    Fix invalid struct member name

--- src/sockets.c
+++ src/sockets.c
@@ -52,5 +52,5 @@ is_address_equal(const struct orvibo_socket *const socket, const char *const ip)
 
 static bool
 is_mac_equal(const struct orvibo_socket *const socket, const unsigned char mac[static const ETHER_ADDR_LEN]) {
-    return socket != NULL && memcmp(socket->mac, mac, ETHER_ADDR_LEN) == 0;
+    return socket != NULL && memcmp(socket->mac_bytes, mac, ETHER_ADDR_LEN) == 0;
 }
